4f0a277b07a91090ae036fb5b3122a642dd1e45d
vithumvindeepa <vindeepavithum@gmail.com>
2024-07-04 21:15:52 +0530

    StatusDTO

---
+++ ticket-service/src/main/java/com/example/ticketservice/dto/StatusDTO.java
@@ -0,0 +1,16 @@
+package com.example.ticketservice.dto;
+
+/**
+ * @author Vithum vindeepa
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class StatusDTO {
+
+    private String id;
+    private String status;
+
+}
+
