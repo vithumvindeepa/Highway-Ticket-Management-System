5b53f20173a9ecd96997df327afbf551099e9328
vithumvindeepa <vindeepavithum@gmail.com>
2024-07-04 21:10:46 +0530

    ticketcontroller

---
+++ service-reg/src/main/java/com/example/servicereg/ServiceRegApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicereg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegApplication.class, args);
+    }
+
+}
